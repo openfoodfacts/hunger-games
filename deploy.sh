e5bc2e7e56f49506993cbb8e607f6edbb325826d
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-06-09 20:27:27 +0200

    add some scipts (autodeploy, dependabot, labeler)

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd build;
+cp index.html 404.html;
+cp index.html insights.html;
+cp index.html questions.html;
+cp index.html eco-score.html;
+cp index.html settings.html;
+cp index.html logos.html;
+cd ..;
