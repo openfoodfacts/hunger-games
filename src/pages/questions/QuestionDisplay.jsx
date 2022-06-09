1b7217a75b200ad768066f0c9fe2694de974450b
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-06-09 23:45:02 +0200

    allow to zoom on maing image

--- src/pages/questions/QuestionDisplay.jsx
+++ src/pages/questions/QuestionDisplay.jsx
@@ -11,6 +11,9 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import DoneIcon from "@mui/icons-material/Done";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+
 import { useTranslation } from "react-i18next";
 import { NO_QUESTION_LEFT, OFF_URL } from "../../const";
 import { reformatValueTag } from "../../utils";
@@ -37,7 +40,7 @@ const QuestionDisplay = ({ question, answerQuestion }) => {
   const valueTagExamplesURL = getValueTagExamplesURL(question);
 
   if (question === NO_QUESTION_LEFT) {
-    return <p>NO Questions</p>;
+    return <p>{t("questions.no_questions_remaining")}</p>;
   }
   if (question === null) {
     return <p>loading</p>;
@@ -56,7 +59,13 @@ const QuestionDisplay = ({ question, answerQuestion }) => {
         </a>
       )}
       <Divider />
-      <img src={question.source_image_url || "https://static.openfoodfacts.org/images/image-placeholder.png"} alt="" />
+      <Zoom>
+        <img
+          src={question.source_image_url.replace("400.jpg", "jpg") || "https://static.openfoodfacts.org/images/image-placeholder.png"}
+          alt=""
+          style={{ maxWidth: "400px", maxHeight: "400px" }}
+        />
+      </Zoom>
       <Stack direction="row" justifyContent="space-between">
         <Button onClick={() => answerQuestion({ value: 0, insightId: question.insight_id })} startIcon={<DeleteIcon />} color="error" variant="contained" size="large">
           {t("questions.no")}
