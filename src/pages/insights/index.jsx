a0431fd08148ea3685e34599567803e8938fe8aa
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-06-19 21:57:32 +0200

    add filter logic links

--- src/pages/insights/index.jsx
+++ src/pages/insights/index.jsx
@@ -5,12 +5,19 @@ import FilterInsights from "./FilterInsights";
 import InsightGrid from "./InsightsGrid";
 
 export default function Insights() {
+  const [filterState, setFilterState] = React.useState({
+    barcode: "",
+    valueTag: "",
+    insightType: "",
+    annotationStatus: "",
+  });
+
   return (
     <Stack spacing={2}>
       <p>Insights page</p>
-      <FilterInsights />
+      <FilterInsights filterState={filterState} setFilterState={setFilterState} />
       <Divider />
-      <InsightGrid />
+      <InsightGrid filterState={filterState} />
     </Stack>
   );
 }
