36072500e985a231e682bcc9020aeddb4793ed4c
sumana2001 <sumana.basu2001@gmail.com>
2022-06-19 18:46:54 +0530

    style: mui system styling added

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,16 +1,46 @@
 import * as React from "react";
-import styles from '../../styles/notFound.module.css'
+import Box from "@mui/material/Box";
 
 export default function Insights() {
   return (
     <center>
-      <img src={require("../../assets/404.png")} className={styles.logo} />
-      <h1 className={styles.text}>
+      <Box
+        component="img"
+        sx={{
+          width: { xs: "100%", md: "60%" },
+          height: "auto",
+        }}
+        alt="The house from the offer."
+        src={require("../../assets/404.png")}
+      />
+      <Box
+        sx={{
+          typography: "h1",
+          fontSize: "2em",
+          marginTop: "-5rem",
+        }}
+      >
         Whoops! The page you're looking for can't be found.
-      </h1>
-      <p className={styles.redirect}>
-        Want to play some games? <a href="/logos">Click here</a>
-      </p>
+      </Box>
+      <Box
+        sx={{
+          typography: "body1",
+          fontSize: "1.2em",
+          marginTop: "1rem",
+        }}
+      >
+        Want to play some games?{" "}
+        <Box
+          component="a"
+          href="/logos"
+          sx={{
+            textDecoration: "none",
+            color: "#6559f6",
+          }}
+        >
+          Click here
+        </Box>
+      </Box>
     </center>
   );
 }
