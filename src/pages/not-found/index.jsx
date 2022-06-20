314b3587ac35e3f944ff3f5c41271e57237f3853
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-06-20 08:46:20 +0200

    feedbacks

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,48 +1,37 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 import { useTranslation } from "react-i18next";
 
 export default function Insights() {
   const { t } = useTranslation();
+
   return (
-    <center>
+    <Box sx={{ textAlign: "center" }}>
       <Box
         component="img"
         sx={{
-          width: { xs: "100%", md: "60%" },
+          width: { xs: "100%", sm: 600 },
           height: "auto",
         }}
         alt="404 not found"
         src={require("../../assets/404.png")}
       />
-      <Box
-        sx={{
-          typography: "h1",
-          fontSize: "2em",
-          marginTop: "-5rem",
-        }}
-      >
-        {t("notfound.nopage")}
-      </Box>
-      <Box
-        sx={{
-          typography: "body1",
-          fontSize: "1.2em",
-          marginTop: "1rem",
-        }}
-      >
-        {t("notfound.redirect1")}
-        <Box
+      <Typography variant="h4">{t("notfound.nopage")}</Typography>
+      <Typography variant="body1" sx={{ mt: 2 }}>
+        {t("notfound.redirect1")}{" "}
+        <Typography
           component="a"
-          href="/logos"
+          href="/questions"
           sx={{
             textDecoration: "none",
             color: "#6559f6",
           }}
         >
           {t("notfound.redirect2")}
-        </Box>
-      </Box>
-    </center>
+        </Typography>
+      </Typography>
+    </Box>
   );
 }
