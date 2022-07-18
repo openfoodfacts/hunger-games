852824c6b0afb973e1a05c99416762d262baca41
sumana2001 <sumana.basu2001@gmail.com>
2022-07-18 13:12:37 +0530

    banner added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,6 +7,7 @@ import Typography from "@mui/material/Typography";
 import HomeCard from "./homeCards";
 import QuestionCard from "../../components/QuestionCard";
 import { localFavorites } from "../../localeStorageManager";
+import { Button } from "@mui/material";
 
 const Home = () => {
   const [savedQuestions] = React.useState(() => {
@@ -15,36 +16,92 @@ const Home = () => {
   const size = Object.keys(savedQuestions).length;
 
   return (
-    <Box sx={{ p: 2, alignItems: "center" }}>
-      <Typography
-        variant="h5"
-        sx={{ margin: "20px auto", textAlign: "center" }}
-      >
-        What game would you like to play?
-      </Typography>
-      <HomeCard />
-      {size > 0 && (
+    <>
+      <Box sx={{ p: 2, alignItems: "center" }}>
         <Typography
           variant="h5"
-          sx={{ margin: "40px auto", textAlign: "center" }}
+          sx={{ margin: "20px auto", textAlign: "center" }}
         >
-          Saved Filters
+          What game would you like to play?
         </Typography>
-      )}
-      <Stack
-        spacing={4}
-        flexWrap="wrap"
-        direction={{ xs: "column", sm: "column", md:"row" }}
-        alignItems="center"
-        justifyContent="center"
+        <HomeCard />
+        {size > 0 && (
+          <Typography
+            variant="h5"
+            sx={{ margin: "40px auto", textAlign: "center" }}
+          >
+            Saved Filters
+          </Typography>
+        )}
+        <Stack
+          spacing={4}
+          flexWrap="wrap"
+          direction={{ xs: "column", sm: "column", md: "row" }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          {savedQuestions.map((props) => (
+            <Box key={props.title}>
+              <QuestionCard showFilterResume {...props} />
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+      <Box
+        padding={{ xs: "20px", sm: "50px" }}
+        sx={{
+          backgroundColor: "#4fc3f7",
+        }}
       >
-        {savedQuestions.map((props) => (
-          <Box key={props.title}>
-            <QuestionCard showFilterResume {...props} />
-          </Box>
-        ))}
-      </Stack>
-    </Box>
+        <Stack
+          spacing={4}
+          flexWrap="wrap"
+          direction={{ xs: "column", sm: "column", md: "row" }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography
+            variant="h5"
+            sx={{
+              textAlign: "center",
+              lineHeight: "1.75",
+            }}
+          >
+            Want to make your contribution count? Create an account or sign in
+            on Open Food Facts!
+          </Typography>
+          <Stack direction="row">
+            <Button
+              variant="contained"
+              href="https://world.openfoodfacts.org/cgi/login.pl"
+              sx={{
+                backgroundColor: "white",
+                color: "black",
+                margin: "0 20px",
+                "&:hover": {
+                  backgroundColor: "#F5F5F5",
+                },
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              href="https://world.openfoodfacts.org/cgi/user.pl"
+              sx={{
+                backgroundColor: "white",
+                color: "black",
+                "&:hover": {
+                  backgroundColor: "#F5F5F5",
+                },
+              }}
+            >
+              Sign up
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+    </>
   );
 };
 
