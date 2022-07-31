b63c8666fc356180d34648359d0285481ba708e6
sumana2001 <sumana.basu2001@gmail.com>
2022-07-31 17:19:08 +0530

    home page added

---
+++ src/pages/home/homeCards.jsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActionArea from "@mui/material/CardActionArea";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import home1 from "../../assets/home1.png";
+import home2 from "../../assets/home2.png";
+import home3 from "../../assets/home3.png";
+
+const content = [
+  {
+    title: "Questions",
+    desc: "Tell us if the given image answers the questions",
+    link: "/questions",
+    image: home1,
+  },
+  {
+    title: "Logos",
+    desc: "Find all the logos matching the given logo",
+    link: "/logos",
+    image: home2,
+  },
+  {
+    title: "Eco-score",
+    desc: "Annotate the questions with the highest value",
+    link: "/eco-score",
+    image: home3,
+  },
+];
+
+const HomeCards = () => {
+  return (
+    <Stack
+      spacing={3}
+      direction={{ xs: "column", sm: "column", md: "row" }}
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        marginBottom: "30px",
+      }}
+    >
+      {content.map((props) => (
+        <Card sx={{ width: 350, height: 300 }} key={props.title}>
+        <CardActionArea component={Link} to={props.link}>     
+            <CardMedia
+              component="img"
+              height="200"
+              image={props.image}
+              alt=""
+              sx={{ objectFit: "contain" }}
+            />
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {props.title}
+              </Typography>
+              <Typography
+                gutterBottom
+                variant="p"
+                component="div"
+                fontSize={14}
+              >
+                {props.desc}
+              </Typography>
+            </CardContent>
+            </CardActionArea>
+          </Card>
+      ))}
+    </Stack>
+  );
+};
+
+export default HomeCards;
