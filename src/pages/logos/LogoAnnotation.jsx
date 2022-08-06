c04c01882a00dc5cee2b9aef1bacd749ebbea238
Yashasvi Chaurasia <yashasvi12977@gmail.com>
2022-08-06 19:00:12 +0530

    updated explanation text

--- src/pages/logos/LogoAnnotation.jsx
+++ src/pages/logos/LogoAnnotation.jsx
@@ -216,23 +216,7 @@ export default function LogoAnnotation() {
       >
         {t("logos.annotations")}
       </Box>
-      <h3>How to Play</h3>
-      <ol>
-        <li>Select all The Logos from the logo grid that look similar</li>
-        <li>
-          You can only select Logos which are <strong>NOT</strong> highlighted
-          in grey
-        </li>
-        <li>
-          After the selection, in the 'Value' box below fill the common value in
-          all logos.{" "}
-        </li>
-        <li>
-          Proceed and select the 'Type' box with the type of value you have
-          filled{" "}
-        </li>
-        <li>Click on the 'Update' to Annotate the Logos and Your Done !</li>
-      </ol>
+      <p>Select all logos that are exactly the same, and give them a name like "en:EU Organic" and a type like "label".</p>
       <LogoForm
         // TODO: if the logoSearchParams.logo_id is defined and the first logo ios labelised, values should by default be initialized with its values
         value=""
