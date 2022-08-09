79d4643b98e99fe96272259bef59b2265396c69a
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-08-09 22:44:12 +0200

    remove unused import

--- src/pages/nutrition/index.jsx
+++ src/pages/nutrition/index.jsx
@@ -2,43 +2,47 @@ import * as React from "react";
 import NutritionTable from "./table";
 import ProductNutriments from "./productCard";
 import { Box } from "@mui/material";
-import { flexbox } from "@material-ui/system";
 
-import {basicNutriments} from "./nutritionFields";
+import { basicNutriments } from "./nutritionFields";
 
 export default function Nutrition() {
-
-  const [nutriments, setNutriments] = React.useState(basicNutriments)
+  const [nutriments, setNutriments] = React.useState(basicNutriments);
 
   function onchangeHandler(e) {
-    const {value, name} = e.target
-    setNutriments(prevState => prevState.map(
-      nutr => {
-        return name === nutr.label? {...nutr, value} : nutr
-      }
-    ))
+    const { value, name } = e.target;
+    setNutriments((prevState) =>
+      prevState.map((nutr) => {
+        return name === nutr.label ? { ...nutr, value } : nutr;
+      })
+    );
   }
 
   function deleteItem(nutr) {
-    setNutriments(prev => prev.map(nutriment => nutriment === nutr ? ({...nutriment, display: false}) : nutriment ))
+    setNutriments((prev) =>
+      prev.map((nutriment) =>
+        nutriment === nutr ? { ...nutriment, display: false } : nutriment
+      )
+    );
   }
 
   return (
-    <Box display={"flex"}
-         flexDirection={{ xs: "column", md: "row" }}
-         gap={2}
-         sx={{
-           width: 1,
-           height: 1,
-           alignItems: { xs: 'center', md:"flex-start" },
-           justifyContent: "center",
-           padding: 4
-         }}>
-      <ProductNutriments setNutriments={setNutriments}/>
+    <Box
+      display={"flex"}
+      flexDirection={{ xs: "column", md: "row" }}
+      gap={2}
+      sx={{
+        width: 1,
+        height: 1,
+        alignItems: { xs: "center", md: "flex-start" },
+        justifyContent: "center",
+        padding: 4,
+      }}
+    >
+      <ProductNutriments setNutriments={setNutriments} />
       <NutritionTable
-        nutriments={nutriments.filter(nutr => nutr.display)}
+        nutriments={nutriments.filter((nutr) => nutr.display)}
         setNutriments={setNutriments}
-        additionalNutriments={nutriments.filter(nutr => !nutr.display)}
+        additionalNutriments={nutriments.filter((nutr) => !nutr.display)}
         onchangeHandler={onchangeHandler}
         deleteItem={deleteItem}
       />
