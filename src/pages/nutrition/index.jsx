81a5cc941676f06908e075386b2af4d6568a5484
Shmigolk <75388692+Shmigolk@users.noreply.github.com>
2022-08-15 15:17:15 +0300

    feature: add nutrition page and display product card with temporary borders

---
+++ src/pages/nutrition/index.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+// import NutritionTable from "./table";
+import ProductNutriments from "./productCard";
+import { Box } from "@mui/material";
+
+// import { basicNutriments } from "./nutritionFields";
+
+export default function Nutrition() {
+  /*const [nutriments, setNutriments] = React.useState(basicNutriments);
+
+  function onchangeHandler(e) {
+    const { value, name, id } = e.target;
+    console.log(name);
+    setNutriments((prevState) =>
+      prevState.map((nutr) => {
+        return id === nutr.label ? { ...nutr, [name]: value } : nutr;
+      })
+    );
+  }
+
+  function deleteItem(nutr) {
+    setNutriments((prev) =>
+      prev.map((nutriment) =>
+        nutriment === nutr ? { ...nutriment, display: false } : nutriment
+      )
+    );
+  }*/
+
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={{ xs: "column", md: "row" }}
+      gap={2}
+      sx={{
+        width: 1,
+        height: 1,
+        alignItems: { xs: "center", md: "flex-start" },
+        justifyContent: "center",
+        padding: 4,
+        border: "5px solid green",
+        minHeight: "89.4vh",
+      }}
+    >
+      <ProductNutriments setNutriments={{}} nutriments={[]} />
+      {/*<NutritionTable*/}
+      {/*  nutriments={nutriments.filter((nutr) => nutr.display)}*/}
+      {/*  setNutriments={setNutriments}*/}
+      {/*  additionalNutriments={nutriments.filter((nutr) => !nutr.display)}*/}
+      {/*  onchangeHandler={onchangeHandler}*/}
+      {/*  deleteItem={deleteItem}*/}
+      {/*/>*/}
+    </Box>
+  );
+}
