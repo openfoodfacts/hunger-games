c9a78df6ed8d7e645507db742da66340fcc4d949
Konstantin <75388692+Shmigolk@users.noreply.github.com>
2022-08-21 16:22:21 +0300

    feat: Nutrition game - table, tableRow and select Unit components (#154)
    Co-authored-by: Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>

--- src/pages/nutrition/index.jsx
+++ src/pages/nutrition/index.jsx
@@ -1,8 +1,30 @@
 import * as React from "react";
+import NutritionTable from "./table";
 import ProductNutriments from "./productCard";
 import { Box } from "@mui/material";
 
+import { basicNutriments } from "./nutritionFields";
+
 export default function Nutrition() {
+  const [nutriments, setNutriments] = React.useState(basicNutriments);
+
+  function onchangeHandler(e) {
+    const { value, name, id } = e.target;
+    setNutriments((prevState) =>
+      prevState.map((nutr) => {
+        return id === nutr.label ? { ...nutr, [name]: value } : nutr;
+      })
+    );
+  }
+
+  function deleteItem(nutr) {
+    setNutriments((prev) =>
+      prev.map((nutriment) =>
+        nutriment === nutr ? { ...nutriment, display: false } : nutriment
+      )
+    );
+  }
+
   return (
     <Box
       flexDirection={{ xs: "column", md: "row" }}
@@ -18,7 +40,16 @@ export default function Nutrition() {
         minHeight: "89.4vh",
       }}
     >
-      <ProductNutriments setNutriments={{}} nutriments={[]} />
+      <ProductNutriments
+        setNutriments={setNutriments}
+        nutriments={nutriments}
+      />
+      <NutritionTable
+        nutriments={nutriments.filter((nutr) => nutr.display)}
+        setNutriments={setNutriments}
+        onchangeHandler={onchangeHandler}
+        deleteItem={deleteItem}
+      />
     </Box>
   );
 }
---
+++ src/pages/nutrition/table.jsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { Box } from "@mui/material";
+// import AdditionalNutriments from "./additionalNutritions";
+import { useTranslation } from "react-i18next";
+
+import TableRowComp from "./tableRow";
+
+export default function NutritionTable({
+  nutriments,
+  setNutriments,
+  additionalNutriments,
+  deleteItem,
+  onchangeHandler,
+}) {
+  const { t } = useTranslation();
+
+  const rows = nutriments.map((nutriment) => (
+    <TableRowComp
+      nutriment={nutriment}
+      onchangeHandler={onchangeHandler}
+      deleteItem={deleteItem}
+      key={nutriment.off_nutriment_id}
+    />
+  ));
+  // const options = additionalNutriments.map((item) => ({
+  //   label: t(`nutrition.nutriments.${item.label}`),
+  //   id: item.label,
+  // }));
+
+  return (
+    <Box>
+      <TableContainer
+        sx={{
+          margin: 0,
+          maxWidth: "1000px",
+          borderRadius: "2%",
+          backgroundColor: "#fdf5e6",
+        }}
+      >
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell
+                sx={{
+                  fontSize: "large",
+                  fontWeight: "bold",
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "100%",
+                }}
+              >
+                <Box
+                  display={"flex"}
+                  flexDirection={"row"}
+                  sx={{ alignItems: "center" }}
+                >
+                  {t("nutrition.table.value")}
+                  {/*<AdditionalNutriments*/}
+                  {/*  options={options}*/}
+                  {/*  setNutriments={setNutriments}*/}
+                  {/*/>*/}
+                </Box>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{rows}</TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
