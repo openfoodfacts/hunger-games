578225a8ce9e8ed4b789312da89712df74149f1c
Alexandre Fauquette <alex.fauquette@gmail.com>
2022-08-21 15:54:48 +0200

    Use directly textfields in nutriment row

--- src/pages/nutrition/tableRow.jsx
+++ src/pages/nutrition/tableRow.jsx
@@ -2,11 +2,20 @@ import * as React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import TextField from "@mui/material/TextField";
-import SelectUnit from "./unitSelect";
+import MenuItem from "@mui/material/MenuItem";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 import { useTranslation } from "react-i18next";
 
+const defaultUnitOptions = [
+  { value: "g", label: "g" },
+  { value: "mg", label: "mg" },
+];
+const qualificationOptions = [
+  { value: "=", label: "=" },
+  { value: ">", label: ">" },
+  { value: "<", label: "<" },
+];
 export default function TableRowComp({
   nutriment,
   onchangeHandler,
@@ -16,17 +25,25 @@ export default function TableRowComp({
 
   const nutrimentLabel = t(`nutrition.nutriments.${nutriment.label}`);
 
+  const unitOptions = nutriment.unitOptions ?? defaultUnitOptions;
   return (
     <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
       <TableCell component="th" scope="row">
         {nutrimentLabel}
       </TableCell>
       <TableCell>
-        <SelectUnit
-          options={nutriment.quantification}
-          value={"quantification"}
-          onchangeHandler={onchangeHandler}
-        />
+        <TextField
+          select
+          onChange={onchangeHandler}
+          value={nutriment.quantification}
+          sx={{ minWidth: 80 }}
+        >
+          {qualificationOptions.map(({ label, value }) => (
+            <MenuItem key={label} value={value}>
+              {label}
+            </MenuItem>
+          ))}
+        </TextField>
       </TableCell>
 
       <TableCell sx={{ width: 20 }}>
@@ -46,11 +63,20 @@ export default function TableRowComp({
         />
       </TableCell>
       <TableCell>
-        <SelectUnit
-          options={nutriment.unit}
-          value={"unit"}
-          onchangeHandler={onchangeHandler}
-        />
+        {unitOptions && (
+          <TextField
+            select
+            onChange={onchangeHandler}
+            value={nutriment.unit}
+            sx={{ minWidth: 80 }}
+          >
+            {unitOptions.map(({ label, value }) => (
+              <MenuItem key={label} value={value}>
+                {label}
+              </MenuItem>
+            ))}
+          </TextField>
+        )}
       </TableCell>
       <TableCell>
         <DeleteOutlineIcon
