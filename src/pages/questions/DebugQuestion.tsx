cf0b006dcbcdda91af91edee91ee2d0d7ce95b7f
Alexandre Fauquette <45398769+alexfauquette@users.noreply.github.com>
2022-09-23 22:48:42 +0200

    feat: Show debug informations about insights (#225)

---
+++ src/pages/questions/DebugQuestion.tsx
@@ -0,0 +1,159 @@
+import * as React from "react";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import LinearProgress from "@mui/material/LinearProgress";
+
+import robotoff from "../../robotoff";
+import off from "../../off";
+
+import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+
+const fetchData = async (insightId) => {
+  const response = await robotoff.insightDetail(insightId);
+  return response;
+};
+
+const getCroppedLogoUrl = (debugData) => {
+  if (!debugData?.source_image || !debugData?.data?.bounding_box) {
+    return null;
+  }
+
+  const sourceImage = off.getImageUrl(debugData?.source_image);
+  return robotoff.getCroppedImageUrl(
+    sourceImage,
+    debugData?.data?.bounding_box
+  );
+};
+const DebugQuestion = (props) => {
+  const { insightId } = props;
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [debugData, setDebugData] = React.useState<any>({});
+  const [openDetails, setOpenDetails] = React.useState<any>({
+    resume: false,
+    json_details: false,
+  });
+
+  const handleChange = (panelId) => (event, newState) => {
+    setOpenDetails((prev) => ({ ...prev, [panelId]: newState }));
+  };
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    let isValid = true;
+    fetchData(insightId)
+      .then(({ data }) => {
+        if (!isValid) {
+          return;
+        }
+        setIsLoading(false);
+        setDebugData(data);
+      })
+      .catch((e) => {
+        if (!isValid) {
+          return;
+        }
+        setIsLoading(false);
+        setDebugData(e);
+      });
+    return () => {
+      isValid = false;
+    };
+  }, [insightId]);
+
+  const croppedUrl = getCroppedLogoUrl(debugData);
+  return (
+    <>
+      <Divider sx={{ mt: 2 }} />
+      <Box sx={{ px: 2, my: 2 }}>
+        <Accordion
+          variant="outlined"
+          onChange={handleChange("resume")}
+          expanded={openDetails["resume"]}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>resume</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {isLoading ? (
+              <LinearProgress />
+            ) : (
+              <div>
+                <Table size="small">
+                  <TableBody
+                    sx={{
+                      " td, th": { border: "none" },
+                      th: { verticalAlign: "top", pr: 0 },
+                    }}
+                  >
+                    <TableRow>
+                      <TableCell component="th" scope="row">
+                        insight id
+                      </TableCell>
+                      <TableCell>{debugData?.id}</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                      <TableCell component="th" scope="row">
+                        generator model
+                      </TableCell>
+                      <TableCell>{debugData?.predictor}</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                      <TableCell component="th" scope="row">
+                        timestamp
+                      </TableCell>
+                      <TableCell>
+                        {new Date(debugData?.timestamp).toLocaleString()}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+                {croppedUrl && (
+                  <img alt="lroUsed for prediction" src={croppedUrl} />
+                )}
+              </div>
+            )}
+          </AccordionDetails>
+        </Accordion>
+        <Accordion
+          variant="outlined"
+          onChange={handleChange("json_details")}
+          expanded={openDetails["json_details"]}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>raw JSON</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {isLoading ? (
+              <LinearProgress />
+            ) : (
+              <Typography variant="caption" component="pre">
+                {JSON.stringify(debugData, null, 2)}
+              </Typography>
+            )}
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+    </>
+  );
+};
+
+export default DebugQuestion;
--- src/pages/questions/ProductInformation.jsx
+++ src/pages/questions/ProductInformation.jsx
@@ -26,8 +26,11 @@ import {
   localSettings,
   localSettingsKeys,
   getHideImages,
+  getPageCustomization,
+  getIsDevMode,
 } from "../../localeStorageManager";
 import externalApi from "../../externalApi";
+import DebugQuestion from "./DebugQuestion";
 
 // src looks like: "https://static.openfoodfacts.org/images/products/004/900/053/2258/1.jpg"
 const getImageId = (src) => {
@@ -49,8 +52,13 @@ const getImagesUrls = (images, barcode) => {
 
 const ProductInformation = ({ question }) => {
   const { t } = useTranslation();
+  const isDevMode = getIsDevMode();
+
   const [productData, setProductData] = React.useState({});
   const [hideImages, setHideImages] = React.useState(getHideImages);
+  const [devCustomization, setDevCustomization] = React.useState(
+    () => getPageCustomization().questionPage
+  );
   const [flagged, setFlagged] = React.useState([]);
 
   const flagImage = (src, barcode) => {
@@ -108,9 +116,6 @@ const ProductInformation = ({ question }) => {
 
   if (!question || question.insight_id === NO_QUESTION_LEFT) {
     return null;
-  }
-  if (productData.isLoading) {
-    return <p>loading...</p>;
   }
 
   return (
@@ -186,7 +191,7 @@ const ProductInformation = ({ question }) => {
       {/* Remaining info */}
       <Divider />
 
-      <Table size="small" aria-label="a dense table">
+      <Table size="small">
         <TableBody
           sx={{
             " td, th": { border: "none" },
@@ -219,6 +224,9 @@ const ProductInformation = ({ question }) => {
           </TableRow>
         </TableBody>
       </Table>
+      {isDevMode && devCustomization.showDebug && (
+        <DebugQuestion insightId={question.insight_id} />
+      )}
       <Divider />
     </Box>
   );
