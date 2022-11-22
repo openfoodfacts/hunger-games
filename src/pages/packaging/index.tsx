5fabaf3870273a4c3e27e3e0d76ca0a81e611fe9
alexandre <alex.fauquette@gmail.com>
2022-11-22 23:44:52 +0100

    WIP

--- src/pages/packaging/index.tsx
+++ src/pages/packaging/index.tsx
@@ -11,45 +11,114 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 
+import { useBuffer } from "./useBuffer";
 import { Option, useOptions } from "../../hooks/useOptions";
 import { getLang } from "../../localeStorageManager";
+import { optionGroupUnstyledClasses } from "@mui/base";
 
-const CustomAutoComplet = ({ options }: { options: Option[] }) => {
+const CustomAutoComplet = (props: {
+  options: Option[];
+  value: string;
+  onChange: any;
+}) => {
+  const { options, value, onChange } = props;
   return (
     <Autocomplete
       disablePortal
-      options={options}
       sx={{ width: 300 }}
       renderInput={(params) => <TextField {...params} />}
       filterOptions={(options, { inputValue }) => {
-        console.log(options);
         return options.filter((option) =>
-          option.synonyms.some((synonym) => synonym.startsWith(inputValue))
+          option.synonyms.some((synonym) =>
+            synonym.toLowerCase().includes(inputValue.toLowerCase())
+          )
         );
       }}
+      isOptionEqualToValue={(option, value) => option.value === value.value}
+      options={options}
+      value={value && { value, synonyms: [] }}
+      onChange={onChange}
     />
   );
 };
 
 const Row = (props) => {
-  const { packagingMaterials, packagingShapes, packagingRecycling } = props;
+  const {
+    packagingMaterials,
+    packagingShapes,
+    packagingRecycling,
+    material,
+    number,
+    recycling,
+    shape,
+  } = props;
+
+  const [innerMaterial, setInnerMaterial] = React.useState(material);
+  React.useEffect(() => {
+    setInnerMaterial(material);
+  }, [material]);
+
+  const [innerNumber, setInnerNumber] = React.useState(number);
+  React.useEffect(() => {
+    setInnerNumber(number);
+  }, [number]);
+
+  const [innerRecycling, setInnerRecycling] = React.useState(recycling);
+  React.useEffect(() => {
+    setInnerRecycling(recycling);
+  }, [recycling]);
+
+  const [innerShape, setInnerShape] = React.useState(shape);
+  React.useEffect(() => {
+    setInnerShape(shape);
+  }, [shape]);
 
   return (
     <TableRow>
       <TableCell>
-        <TextField />
+        <TextField
+          value={innerNumber}
+          onChange={(event) => setInnerNumber(event.target.value)}
+        />
       </TableCell>
 
       <TableCell>
-        <CustomAutoComplet options={packagingShapes} />
+        <CustomAutoComplet
+          options={packagingShapes}
+          value={innerMaterial}
+          onChange={(event, newValue) => {
+            if (newValue === null) {
+              setInnerMaterial(null);
+            }
+            setInnerMaterial(newValue.value);
+          }}
+        />
       </TableCell>
 
       <TableCell>
-        <CustomAutoComplet options={packagingMaterials} />
+        <CustomAutoComplet
+          options={packagingMaterials}
+          value={innerMaterial}
+          onChange={(event, newValue) => {
+            if (newValue === null) {
+              setInnerMaterial(null);
+            }
+            setInnerMaterial(newValue.value);
+          }}
+        />
       </TableCell>
 
       <TableCell>
-        <CustomAutoComplet options={packagingRecycling} />
+        <CustomAutoComplet
+          options={packagingRecycling}
+          value={innerMaterial}
+          onChange={(event, newValue) => {
+            if (newValue === null) {
+              setInnerMaterial(null);
+            }
+            setInnerMaterial(newValue.value);
+          }}
+        />
       </TableCell>
     </TableRow>
   );
@@ -60,7 +129,18 @@ const Page = (props) => {
   const packagingShapes = useOptions("packaging_shapes", lang);
   const packagingRecycling = useOptions("packaging_recycling", lang);
 
-  const [rows, setRows] = React.useState([{ id: 1 }]);
+  const [rows, setRows] = React.useState([]);
+
+  const data = useBuffer();
+
+  React.useEffect(() => {
+    if (data[0]) {
+      setRows(
+        data[0].packagings.map((item, index) => ({ id: index, ...item }))
+      );
+    }
+  }, [data[0]?.code]);
+
   return (
     <>
       <TableContainer component={Paper}>
