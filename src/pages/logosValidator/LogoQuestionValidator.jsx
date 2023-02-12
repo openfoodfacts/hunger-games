4699c473dd47e16a91d520eea411a3f689f1187a
alexandre <alex.fauquette@gmail.com>
2023-02-12 21:55:45 +0100

    redirect question logos to logo deep search

--- src/pages/logosValidator/LogoQuestionValidator.jsx
+++ src/pages/logosValidator/LogoQuestionValidator.jsx
@@ -124,6 +124,29 @@ const LogoQuesitonCard = (props) => {
   );
 };
 
+const NoMoreLogos = ({ insightType, valueTag }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Box sx={{ width: "100%", textAlign: "center", py: 5, m: 0 }}>
+      <Typography variant="subtitle1">
+        {t("logos.no_more_questions")}
+      </Typography>
+      <Button
+        color="secondary"
+        size="small"
+        component={Link}
+        variant="contained"
+        href={`/logos/deep-search?type=${insightType}&value=${valueTag}`}
+        target="_blank"
+        sx={{ ml: 2, minWidth: 150 }}
+      >
+        Search
+      </Button>
+    </Box>
+  );
+};
+
 export default function LogoQuestionValidator({ predictor }) {
   const { t } = useTranslation();
 
@@ -339,6 +362,8 @@ export default function LogoQuestionValidator({ predictor }) {
             />
           ))}
       </div>
+
+      {remainingQuestionNb === 0 && <NoMoreLogos {...filterState} />}
 
       <Paper
         sx={{
