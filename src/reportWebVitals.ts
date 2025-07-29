2aa102ea65b4c92d45a4846e9e0b8dea5c030ad2
VaiTon <eyadlorenzo@gmail.com>
2025-07-29 21:14:43 +0200

    chore: remove any usage in reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,17 @@
 const reportWebVitals = async (
-  onPerfEntry: undefined | ((value: any) => void) = undefined,
+  onPerfEntry: undefined | ((value: unknown) => void) = undefined,
 ) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    const { onCLS, onFID, onFCP, onLCP, onTTFB } = await import("web-vitals");
-
-    onCLS(onPerfEntry);
-    onFID(onPerfEntry);
-    onFCP(onPerfEntry);
-    onLCP(onPerfEntry);
-    onTTFB(onPerfEntry);
+  if (!onPerfEntry || typeof onPerfEntry !== "function") {
+    return;
   }
+
+  const { onCLS, onFID, onFCP, onLCP, onTTFB } = await import("web-vitals");
+
+  onCLS(onPerfEntry);
+  onFID(onPerfEntry);
+  onFCP(onPerfEntry);
+  onLCP(onPerfEntry);
+  onTTFB(onPerfEntry);
 };
 
 export default reportWebVitals;
