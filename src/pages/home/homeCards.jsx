7ba60f7853cf8fefc2b382122c8d5a463b4cd6f8
VaiTon <eyadlorenzo@gmail.com>
2025-10-03 14:56:10 +0200

    chore: Replace react-router-dom with react-router
    Since v7, react-router-dom is just a wrapper for react-router

--- src/pages/home/homeCards.jsx
+++ src/pages/home/homeCards.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
