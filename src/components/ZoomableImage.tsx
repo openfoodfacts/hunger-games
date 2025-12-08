2d70489c5c618ea8ef4af602256c5ed6ca0b836a
Ramneet04 <ramneetsinghrtd@gmail.com>
2025-12-08 16:36:08 +0530

    Fix ZoomableImage modal: overlay full/compressed button with loader

--- src/components/ZoomableImage.tsx
+++ src/components/ZoomableImage.tsx
@@ -19,6 +19,7 @@ import RotateRightIcon from "@mui/icons-material/RotateRight";
 import OpenInFullIcon from "@mui/icons-material/OpenInFull";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
+import Loader from "../pages/loader";
 
 type ZoomableImageProps = {
   src: string;
@@ -37,9 +38,17 @@ const ZoomableImage = ({
   const apiRef = React.useRef<ReactZoomPanPinchRef>(null);
   const [rotation, setRotation] = React.useState(0);
   const [isOpen, setIsOpen] = React.useState(false);
+  const [useFullResolution, setUseFullResolution] = React.useState(false);
+  const [loadingFullResolution, setLoadingFullResolution] =
+    React.useState(false);
 
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  React.useEffect(() => {
+    setUseFullResolution(false);
+    setLoadingFullResolution(false);
+  }, [src]);
 
   return (
     <>
@@ -72,41 +81,88 @@ const ZoomableImage = ({
         open={isOpen}
         onClose={() => {
           setIsOpen(false);
+          setUseFullResolution(false);
+          setLoadingFullResolution(false);
         }}
         maxWidth="xl"
         fullScreen={fullScreen}
       >
-        <Box
-          sx={{
-            p: 1,
-            display: "flex",
-            flexDirection: "row-reverse",
-          }}
-        >
-          <IconButton onClick={() => setIsOpen(false)}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
+        {loadingFullResolution && (
+          <Box
+            sx={{
+              position: "absolute",
+              inset: 0,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "rgba(0, 0, 0, 0.6)",
+              zIndex: 10,
+            }}
+          >
+            <IconButton onClick={() => setIsOpen(false)}>
+              <CloseIcon />
+            </IconButton>
+            <Loader />
+          </Box>
+        )}
         <Divider />
         <DialogContent
           sx={{
             p: { xs: 1, md: 2 },
+            position: "relative",
           }}
         >
           <TransformWrapper limitToBounds={false} ref={apiRef}>
             <TransformComponent>
               <img
-                src={srcFull ?? src}
+                src={useFullResolution && srcFull ? srcFull : src}
                 alt=""
+                loading="lazy"
                 style={{
-                  maxHeight: "calc(100vh - 160px )",
-                  maxWidth: "min(100%, 1400px)",
+                  maxHeight: "calc(100vh - 160px)",
+                  maxWidth: "100%",
+                  width: "100%",
+                  height: "auto",
+                  objectFit: "contain",
                   transform: `rotate(${rotation * 90}deg)`,
                   transformOrigin: "center",
+                  display: "block",
+                  margin: "auto",
                 }}
               />
             </TransformComponent>
           </TransformWrapper>
+          {srcFull && (
+            <Button
+              disabled={loadingFullResolution}
+              variant={useFullResolution ? "contained" : "outlined"}
+              onClick={() => {
+                if (!useFullResolution && srcFull) {
+                  setLoadingFullResolution(true);
+                  const img = new Image();
+                  img.src = srcFull;
+                  img.onload = () => {
+                    setLoadingFullResolution(false);
+                    setUseFullResolution(true);
+                  };
+                } else {
+                  setUseFullResolution(false);
+                }
+              }}
+              sx={{
+                position: "absolute",
+                bottom: 16,
+                right: 16,
+                zIndex: 10,
+                backgroundColor: alpha(theme.palette.background.paper, 0.7),
+                "&:hover": {
+                  backgroundColor: alpha(theme.palette.background.paper, 0.9),
+                },
+              }}
+            >
+              {useFullResolution ? "Compressed" : "Load Original"}
+            </Button>
+          )}
         </DialogContent>
         <DialogActions>
           <Button
