aa5a50c2f0e70a46e64974ef58e8facacf9b5a28
Ramneet04 <ramneetsinghrtd@gmail.com>
2025-12-11 15:51:25 +0530

    fix: display a human readable message if robotoff is down

--- src/pages/nutrition/index.tsx
+++ src/pages/nutrition/index.tsx
@@ -4,14 +4,18 @@ import { ErrorBoundary } from "../../components/ErrorBoundary";
 import Instructions from "./Instructions";
 import { RobotoffNutrientExtraction } from "../../components/OffWebcomponents";
 import { Box } from "@mui/material";
+import { useRobotoffPredictions } from "./useRobotoffPredictions";
 
 export default function Nutrition() {
+  const { error } = useRobotoffPredictions(false);
+  console.log(error);
   return (
     <React.Suspense>
       <ErrorBoundary>
         <Instructions />
         <Box sx={{ mb: 2, px: 2 }}>
-          <RobotoffNutrientExtraction />
+          {error && error}
+          {!error && <RobotoffNutrientExtraction />}
         </Box>
       </ErrorBoundary>
     </React.Suspense>
