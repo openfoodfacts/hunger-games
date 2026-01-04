a110f672265a2d498b2d559a275403be9224f3b9
Ramneet Singh <144323012+Ramneet04@users.noreply.github.com>
2026-01-04 18:48:01 +0530

    feat: switch Hunger Games image reporting/flagging to Nutri-Patrol #1036 (#1381)

--- src/externalApi.ts
+++ src/externalApi.ts
@@ -1,11 +1,23 @@
+import { NUTRI_PATROL_URL } from "./const";
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const addImageFlag = (opts: {
   barcode: string;
-  imgid: string;
-  url: string;
+  imgid: number;
 }) => {
-  // TODO: Replace this with NutriPatrol
-  throw new Error("This function is not implemented yet");
+  const {barcode, imgid} = opts;
+  const imgidStr = imgid?.toString() || '';
+  const NutriPatrolURL = `${NUTRI_PATROL_URL}?` +
+  new URLSearchParams({
+    barcode: barcode || '',
+    image_id: imgidStr || '',
+    source: "web", flavor: "off",
+  }).toString();
+  try {
+    window.open(NutriPatrolURL, '_blank', 'noopener,noreferrer');
+  } catch (error) {
+    throw new Error("Could not open NutriPatrol, error: " + error);
+  }
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
