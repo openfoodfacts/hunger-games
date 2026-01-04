27648d81ade29da4be655e08f6b0aa63d0395a54
VaiTon <eyadlorenzo@gmail.com>
2026-01-04 19:10:49 +0100

    fix: add type annotations to ErrorBoundary component for improved type safety

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
 
-export class ErrorBoundary extends React.Component {
-  constructor(props) {
+type ErrorState = { hasError: boolean };
+type ErrorBoundaryProps = {
+  children: React.ReactNode;
+};
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorState
+> {
+  constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError(): Partial<ErrorState> {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
+  componentDidCatch?(error: Error, errorInfo: React.ErrorInfo) {
     // Example "componentStack":
     //   in ComponentThatThrows (created by App)
     //   in ErrorBoundary (created by App)
     //   in div (created by App)
     //   in App
-    console.log(error, info.componentStack);
+    console.log(error, errorInfo.componentStack);
   }
 
   render() {
