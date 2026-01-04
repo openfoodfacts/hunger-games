8d2583467fec81d747f811f0c01244958e765859
VaiTon <eyadlorenzo@gmail.com>
2026-01-04 14:12:56 +0100

    chore: Use defineConfig and apply flat react-hooks config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,15 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -20,4 +19,5 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  reactHooks.configs.flat.recommended,
 ]);
