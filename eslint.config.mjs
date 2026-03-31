22aedb123931f8ab18e07729081c5ab429bb9d42
alex <alex.fauquette@gmail.com>
2026-03-31 09:06:04 +0200

    cleanup package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -12,6 +13,7 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.vite,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
