49d24560f12eca4df4983c0abf47416c92409f41
benbenben2 <110821832+benbenben2@users.noreply.github.com>
2026-04-12 12:56:02 +0200

    feat/nutrition-country-filter

--- src/components/OffWebcomponents.tsx
+++ src/components/OffWebcomponents.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
 import { OFF_IMAGE_URL, OFF_URL, ROBOTOFF_API_URL } from "../const";
-import { useCountry } from "../contexts/CountryProvider";
-import { getCountryLanguageCode } from "../pages/nutrition/utils";
+import { useTranslation } from "react-i18next";
 import "@openfoodfacts/openfoodfacts-webcomponents";
 
-const useLanguageCode = () => {
-  const [country]: [string, unknown] = useCountry();
-
-  return getCountryLanguageCode(country);
-};
-
 export const OffWebcomponentsConfiguration = () => {
-  const languageCode = useLanguageCode();
+  const { i18n } = useTranslation();
+  // Ensure we have a valid 2-letter language code
+  const languageCode = i18n.language?.substring(0, 2) || "en";
 
   const robotoffConfiguration = JSON.stringify({
     apiUrl: ROBOTOFF_API_URL,
@@ -23,23 +18,40 @@ export const OffWebcomponentsConfiguration = () => {
       robotoff-configuration={robotoffConfiguration}
       openfoodfacts-api-url={OFF_URL}
       language-code={languageCode}
-      assets-images-path="/assets/webcomponents"
     ></off-webcomponents-configuration>
   );
 };
 
 export const RobotoffNutrientExtraction = ({
   productCode,
+  countryCode,
 }: {
   productCode?: string;
+  countryCode?: string;
 }) => {
+  const setRef = React.useCallback(
+    (element: any) => {
+      if (element) {
+        // Set the property directly on the element
+        if (countryCode) {
+          element.countryCode = countryCode;
+          element.setAttribute("country-codes", countryCode);
+        } else {
+          element.countryCode = undefined;
+          element.removeAttribute("country-codes");
+        }
+      }
+    },
+    [countryCode],
+  );
+
   return (
-    <div>
-      <robotoff-nutrient-extraction
-        display-product-link
-        product-code={productCode}
-      ></robotoff-nutrient-extraction>
-    </div>
+    <robotoff-nutrient-extraction
+      ref={setRef}
+      display-product-link
+      product-code={productCode}
+      key={`${productCode}-${countryCode || "all"}`}
+    ></robotoff-nutrient-extraction>
   );
 };
 export const RobotoffIngredientSpellcheck = () => {
