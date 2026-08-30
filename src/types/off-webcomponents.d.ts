e35fc2b90954b0c4f490047f80940f3fd59ca748
VaiTon <eyadlorenzo@gmail.com>
2026-08-30 12:24:41 +0200

    fix(types): declare custom elements for React JSX

--- src/types/off-webcomponents.d.ts
+++ src/types/off-webcomponents.d.ts
@@ -1,38 +1,41 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "off-webcomponents-configuration": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & {
-        "robotoff-configuration"?: string;
-        "openfoodfacts-api-url"?: string;
-        languageCode?: string;
-      },
-      HTMLElement
-    >;
+declare namespace React {
+  namespace JSX {
+    interface IntrinsicElements {
+      "off-webcomponents-configuration": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          "robotoff-configuration"?: string;
+          "openfoodfacts-api-url"?: string;
+          "language-code"?: string;
+          "assets-images-path"?: string;
+        },
+        HTMLElement
+      >;
 
-    "robotoff-nutrient-extraction": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & {
-        "product-code"?: string;
-        "country-codes"?: string;
-        "language-codes"?: string;
-        "display-product-link"?: boolean;
-      },
-      HTMLElement
-    >;
-    "robotoff-ingredient-spellcheck": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
-    "robotoff-ingredient-detection": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
-    "donation-banner": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
-    "mobile-badges": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
+      "robotoff-nutrient-extraction": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          "product-code"?: string;
+          "country-codes"?: string;
+          "language-codes"?: string;
+          "display-product-link"?: boolean;
+        },
+        HTMLElement
+      >;
+      "robotoff-ingredient-spellcheck": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+      "robotoff-ingredient-detection": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+      "donation-banner": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+      "mobile-badges": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
   }
 }
