d40c90feac9ec27617e5d5e26291bf8f94d3126d
Pierre Slamich <pierre@openfoodfacts.org>
2026-09-16 08:18:14 +0200

    reverso

--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -79,6 +79,10 @@ const PAGES: Page[] = [
     translationKey: "menu.nutritions",
     desktopOnly: true,
   },
+  {
+    url: "reverso",
+    translationKey: "menu.reverso",
+  },
   { translationKey: "menu.manage" },
   {
     translationKey: "menu.ingredients",
