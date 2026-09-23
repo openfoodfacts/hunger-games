c59e6cee8233622aa4dd2b13845fe09d40ceae0c
Pierre Slamich <pierre.slamich@gmail.com>
2026-09-23 10:05:37 +0200

    Apply suggestion from @teolemon

--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -52,7 +52,6 @@ const PAGES: Page[] = [
   { translationKey: "menu.games" },
   { url: "questions", translationKey: "menu.questions" },
   { url: "green-score", translationKey: "menu.green-score" },
-  { url: "brandinator", translationKey: "menu.brandinator" },
   {
     translationKey: "menu.logos",
     children: [
